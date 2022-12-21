78dedc0651f1d1834bec83577b61033f6a99ef37
Bartosz Matlak <bartosz.matlak@op.pl>
2022-12-21 13:26:25 +0100

    clean homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
 "use client";
-import { StyledButton } from "../components/StyledButton/StyledButton";
-import { useAppDispatch } from "../store";
-import { toggleTheme } from "../store/themeSlice";
 
 export default function Home() {
-  const dispatch = useAppDispatch();
-  const handleChangeTheme = () => dispatch(toggleTheme());
-  return (
-    <div>
-      <StyledButton onClick={handleChangeTheme}>xD</StyledButton>
-    </div>
-  );
+  return <div></div>;
 }
