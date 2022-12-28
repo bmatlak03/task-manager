4a7cdba70258521391d3c07332d085640ab20d56
Bartosz Matlak <bartosz.matlak@op.pl>
2022-12-28 14:02:45 +0100

    create common typography components

--- components/UI/Typography/Typography.tsx
+++ components/UI/Typography/Typography.tsx
@@ -1,9 +1,14 @@
 "use client";
 import { styled, Typography, TypographyProps } from "@mui/material";
 
-export const Caption = styled(Typography)<TypographyProps>(({ theme }) => ({
+export const Subtitle = styled(Typography)<TypographyProps>(({ theme }) => ({
   fontWeight: "bold",
   fontSize: 12,
   lineHeight: "15.12px",
-  color: theme.palette.primary.dark,
+}));
+
+export const Title = styled(Typography)<TypographyProps>(({ theme }) => ({
+  fontWeight: "bold",
+  fontSize: 18,
+  lineHeight: "23px",
 }));
