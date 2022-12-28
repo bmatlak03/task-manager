bcb673f15532980834f6f983a66c46a724dc0ad4
Bartosz Matlak <bartosz.matlak@op.pl>
2022-12-28 11:04:44 +0100

    fix plus icon color

--- assets/icons.tsx
+++ assets/icons.tsx
@@ -47,7 +47,7 @@ export const PlusIcon = () => (
   >
     <path
       d="M7.368 12V7.344H12V4.632H7.368V0H4.656V4.632H0V7.344H4.656V12H7.368Z"
-      fill="white"
+      fill="currentColor"
     />
   </svg>
 );
