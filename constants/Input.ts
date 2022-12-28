7ac870e86d1222faee16023d2fd17acf55940245
Bartosz Matlak <bartosz.matlak@op.pl>
2022-12-28 11:11:29 +0100

    create placeholders const

---
+++ constants/Input.ts
@@ -0,0 +1,6 @@
+export const inputPlaceholders = {
+  title: "e.g. Take coffee break",
+  description:
+    "e.g. It’s always good to take a break. This 15 minute break will  recharge the batteries a little.",
+  subtasks: "e.g. Make coffee",
+} as const;
