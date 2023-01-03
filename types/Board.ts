dca602f9ecb00f195ebf7be38332788f622dc876
Bartosz Matlak <bartosz.matlak@op.pl>
2023-01-03 13:07:24 +0100

    add id to every type and make tasks optional

--- types/Board.ts
+++ types/Board.ts
@@ -1,9 +1,11 @@
 export interface SubTask {
+  id: string;
   title: string;
   isCompleted: boolean;
 }
 
 export interface Task {
+  id: string;
   title: string;
   description?: string;
   status: string;
@@ -11,11 +13,13 @@ export interface Task {
 }
 
 export interface BoardColumn {
+  id: string;
   name: string;
-  tasks: Task[];
+  tasks?: Task[];
 }
 
 export interface Board {
+  id: string;
   name: string;
   columns: BoardColumn[];
 }
