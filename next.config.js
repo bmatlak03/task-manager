115f6827ef8daa9faefb3845a37e1b5999ad145f
Bartosz Matlak <bartosz.matlak@op.pl>
2023-01-10 12:05:13 +0100

    reduce material ui icons imports

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  modularizeImports: {
+    "@mui/icons-material": {
+      transform: "@mui/icons-material/{{member}}",
+    },
+  },
   experimental: {
     appDir: true,
   },
